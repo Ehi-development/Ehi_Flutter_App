1d23cea597524fc8983902be9f79090a2adaead0
tadsdcz50 <tadsdcz50@gmail.com>
2020-04-04 20:24:44 +0200

    Issues Fix close #11

--- lib/Pages/IconPageLoader.dart
+++ lib/Pages/IconPageLoader.dart
@@ -8,34 +8,35 @@ import 'package:loading/loading.dart';
 import '../UtilityClass/Theme.dart';
 import 'Home.dart';
 
+import 'package:after_layout/after_layout.dart';
+
 class IconPageLoader extends StatefulWidget{
   @override
   IconPageLoaderState createState() => IconPageLoaderState();
 }
 
-class IconPageLoaderState extends State<IconPageLoader> {
+class IconPageLoaderState extends State<IconPageLoader> with AfterLayoutMixin<IconPageLoader> {
 
   GlobalKey view = GlobalKey();
 
   @override
+  void afterFirstLayout(BuildContext context) {
+    UtilityTools.getLoggedUser().then((result){
+      if(result[0]==""){
+        Navigator.of(context).pushReplacement(CircularRevealRoute(widget: LRPage(),position:getContainerPosition(view)));
+      }else{
+        Navigator.of(context).pushReplacement(CircularRevealRoute(widget: Home(),position:getContainerPosition(view)));
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-
-    LoadContent(context,view);
-
     return StatusBarCleaner(
         key: view,
         gradient: MoobTheme.backgroundGradient,
-        child: Center(child: Loading(indicator: BallPulseIndicator(), size: 100.0)),
+        child: Center(child: Loading(indicator: BallPulseIndicator(
+        ), size: 100.0)),
     );
   }
-}
-
-void LoadContent(context, view) async {
-  UtilityTools.getLoggedUser().then((result){
-    if(result[0]==""){
-      Navigator.of(context).pushReplacement(CircularRevealRoute(widget: LRPage(),position:getContainerPosition(view)));
-    }else{
-      Navigator.of(context).pushReplacement(CircularRevealRoute(widget: Home(),position:getContainerPosition(view)));
-    }
-  });
 }
